ca9200dbec6d2a55d2e917051fe011c7650e800c
Miyagi-de-engineer <t.hayato15@gmail.com>
2021-02-03 23:41:33 +0900

    削除ボタンを追加しました
    内部機能を編集しています

--- views/msg.php
+++ views/msg.php
@@ -31,6 +31,7 @@
             foreach ($viewData as $key => $val) {
                 if ($val['from_user'] == $partnerUserId) {
         ?>
+                    <!-- 相手のメッセージ表示部分 -->
                     <div class="card mb-2" style="width: 90%;">
                         <div class="card-body px-1 py-1 d-flex align-items-center">
                             <div class="" style="display: inline-block;">
@@ -38,17 +39,20 @@
                             </div>
                             <div class="" style="display: inline-block;">
                                 <p><?php echo sanitize($val['msg']); ?></p>
-                                <small class="text-muted"><?php echo sanitize($partnerUserInfo['username']); ?> - 送信日：<?php echo sanitize(date('Y年n月j日', strtotime($val['send_date']))); ?></small>
+                                <small id="delete_btn" class="text-muted mb-2"><?php echo sanitize($partnerUserInfo['username']); ?> - 送信日：<?php echo sanitize(date('Y年n月j日', strtotime($val['send_date']))); ?></small>
+                                <p id="delete_btn" class="text-muted">メッセージを削除<i class="fas fa-times-circle" style="color: orangered; margin-left:5px;"></i></p>
                             </div>
                         </div>
                     </div>
                 <?php
                 } else { ?>
+                    <!-- 自分のメッセージ表示部分 -->
                     <div class="card mb-2" style="width: 90%;float:right;">
                         <div class="card-body px-1 py-1 d-flex align-items-center justify-content-end">
                             <div class="mr-2" style="display: inline-block;">
                                 <p><?php echo sanitize($val['msg']); ?></p>
                                 <small class="d-block text-muted text-right"><?php echo sanitize($myUserInfo['username']); ?> - 送信日：<?php echo sanitize(date('Y年n月j日', strtotime($val['send_date']))); ?></small>
+                                <small id="delete_btn" class="text-muted">メッセージを削除<i class="fas fa-times-circle" style="color: orangered; margin-left:5px;"></i></small>
                             </div>
                             <div class="" style="display: inline-block;">
                                 <img src="<?php echo sanitize($myUserInfo['pic']); ?>" id="avatar" class="" alt="">
